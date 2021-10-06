d1f016c24a631fad7e9cb5173e46d2699b01d363
Evyatar-M <64112773+evyatarMad007@users.noreply.github.com>
2021-10-06 22:36:45 +0300

    section 4 - lesson 22 (endpoints GET all products | single product by id) complete.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import connectDB from './config/db.js'
-import products from './data/products.js';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+
 
 dotenv.config();
 const app = express();
@@ -13,16 +14,7 @@ app.get('/', (req, res) => {
     res.send('api is running...')
 })
 
-// send all products 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-
-// send a single product by id 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
