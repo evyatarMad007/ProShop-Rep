d6db3278f2efa7e640adb40fbd7386be51a50e46
Evyatar-M <64112773+evyatarMad007@users.noreply.github.com>
2021-10-09 22:47:13 +0300

    section 4 - lesson 23/24 (using post man and erorrs middleware has been added. for product :id and 404) complete.

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import { notFound, errorHandler } from './middleWare/errorMiddleWare.js'
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 
 
 dotenv.config();
-const app = express();
-
 connectDB()
+const app = express();
 
 app.get('/', (req, res) => {
     res.send('api is running...')
 })
 
 app.use('/api/products', productRoutes)
+
+// erors 
+app.use(notFound)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
