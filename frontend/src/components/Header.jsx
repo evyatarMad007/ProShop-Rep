91855c69a5eae67421506796f9227848c8ba0869
Evyatar-M <64112773+evyatarMad007@users.noreply.github.com>
2021-10-17 01:44:27 +0300

    lesson 34 - cart component has been updated, cart items || subtotal price and items|| button to proceed to checkout - has been added.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 const Header = () => {
     return (
     <header>
-        <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Navbar bg='info' variant='dark' expand='lg' collapseOnSelect>
             <Container >
                 <LinkContainer to='/'>
                     <Navbar.Brand>ProShop</Navbar.Brand>
@@ -14,13 +14,13 @@ const Header = () => {
                 <Navbar.Toggle aria-controls='basic-navbar-nav'/>
                 
                 <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className="ml-auto">
+                    <Nav className="ms-auto">
                         <LinkContainer to='cart'>
-                            <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+                            <Nav.Link className='mx-3'> <i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
                         </LinkContainer>
 
                         <LinkContainer to='login'>
-                            <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>
+                            <Nav.Link> <i className='fas fa-user'></i> Sign In </Nav.Link>
                         </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './bootstrap.min.css'
+import 'bootswatch/dist/spacelab/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
